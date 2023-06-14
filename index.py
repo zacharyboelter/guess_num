b80906c252d39138338ac090726bfbe9e8edca80
Zachary Boelter <zboelter@gmail.com>
2023-06-14 12:04:56 -0700

    pygame not working

--- index.py
+++ index.py
@@ -1,115 +1,68 @@
-# import random
-# import tkinter as tk
-# from tkinter import ttk, messagebox
-
-# style = ttk.Style()
-
-# style.configure("TLabel", font=("Arial", 14))
-# style.configure("TButton", font=("Arial", 12))
-# style.configure("TEntry", font=("Arial", 12))
-
-# window = tk.Tk()
-# window.title("Guess the Number")
-# window.geometry("300x200")
-# window.resizable(False, False)
-
-# def generate_random_number():
-#     return random.randint(1, 100)
-
-# def check_guess():
-#     guess = int(guess_entry.get())
-#     if guess < number_to_guess:
-#         result_label.config(text = "Too low! Try again.")
-#     elif guess > number_to_guess:
-#         result_label.congig(text = "Too high! Guess lower.")
-#     else:
-#         messagebox.showinfo("Thats a bingo!", f"You correctly guessed the number in {num_guesses} attempts.")
-#         reset_game()
-    
-#     guess_entry.delete(0, tk.END)
-#     guess_entry.focus()
-
-
-
-# def reset_game():
-#     global number_to_guess, num_guesses
-#     number_to_guess = generate_random_number()
-#     num_guesses = 0
-#     result_label.config(text="")
-#     guess_entry.delete(0, tk.END)
-#     guess_entry.focus()
-
-
-# window= tk.Tk()
-# window.title("Guess the Number")
-
-# number_to_guess = generate_random_number()
-# num_guesses = 0
-
-# instruction_label = tk.Label(window, text="Guess a number between 1 and 100:")
-# instruction_label.pack()
-
-# guess_entry = tk.Entry(window)
-# guess_entry.pack()
-# guess_entry.focus()
-
-# check_button = tk.Button(window, text="Check", command=check_guess)
-# check_button.pack()
-
-# result_label = tk.Label(window, text="")
-# result_label.pack()
-
-# reset_game()
-
 import random
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import ttk
+import pygame
+from pygame.locals import *
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+window_width, window_height = 400, 300
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Guess the Number")
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # Generate a random number between 1 and 100
 number_to_guess = random.randint(1, 100)
 
-def check_guess():
-    # Get the player's guess from the entry field
-    guess = guess_entry.get()
+# Font setup
+font = pygame.font.SysFont(None, 32)
 
-    try:
-        # Convert the guess to an integer
-        guess = int(guess)
+def draw_text(text, x, y, color=WHITE):
+    surface = font.render(text, True, color)
+    window.blit(surface, (x, y))
 
-        if guess < number_to_guess:
-            result_label.config(text="Too low!")
-        elif guess > number_to_guess:
-            result_label.config(text="Too high!")
-        else:
-            result_label.config(text="Congratulations! You guessed the number!")
-            guess_entry.config(state="disabled")
-            check_button.config(state="disabled")
+# Game loop
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
 
-    except ValueError:
-        messagebox.showerror("Error", "Invalid input. Please enter a valid number.")
+        if event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                guess = guess_text.strip()
 
-# Create the game window
-window = tk.Tk()
-window.title("Guess the Number")
+                try:
+                    guess = int(guess)
 
-# Change the theme to 'clam' for all ttk elements
-style = ttk.Style()
-style.theme_use("clam")
+                    if guess < number_to_guess:
+                        result = "Too low!"
+                    elif guess > number_to_guess:
+                        result = "Too high!"
+                    else:
+                        result = "Congratulations! You guessed the number!"
+                        running = False
 
-# Create and pack GUI elements using ttk
-instruction_label = ttk.Label(window, text="Guess a number between 1 and 100:")
-instruction_label.pack()
+                except ValueError:
+                    result = "Invalid input. Please enter a valid number."
 
-guess_entry = ttk.Entry(window)
-guess_entry.pack()
-guess_entry.focus()
+    # Clear the window
+    window.fill(BLACK)
 
-check_button = ttk.Button(window, text="Check", command=check_guess)
-check_button.pack()
+    # Draw text and input field
+    draw_text("Guess a number between 1 and 100:", 50, 50)
+    draw_text("Your guess:", 50, 100)
+    guess_text = pygame.draw.rect(window, WHITE, (150, 100, 100, 32), 2)
 
-result_label = ttk.Label(window, text="")
-result_label.pack()
+    # Display the result
+    draw_text(result, 50, 150)
 
-# Start the main event loop
-window.mainloop()
+    # Update the display
+    pygame.display.flip()
+
+# Quit the game
+pygame.quit()
