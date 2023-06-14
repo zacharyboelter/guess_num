0cda383e6a6ebe0f0de5b9adc7e717fc7e93346e
Zachary Boelter <zboelter@gmail.com>
2023-06-14 10:56:17 -0700

    global variables and initial game logic

--- index.py
+++ index.py
@@ -5,3 +5,14 @@ from tkinter import messagebox
 def generate_random_number():
     return random.randint(1, 100)
 
+def check_guess():
+    guess = int(guess_entry.get())
+    if guess < number_to_guess:
+        result_label.config(text='Too low! Try again.')
+
+
+
+def reset_game():
+    global number_to_guess, num_guesses
+    number_to_guess = generate_random_number()
+    num_guesses = 0
