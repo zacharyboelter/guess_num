cf430f8ac71ae6cf9b2d58ec0b1a83f642910c89
Zachary Boelter <zboelter@gmail.com>
2023-06-14 11:34:32 -0700

    GUI

--- index.py
+++ index.py
@@ -8,11 +8,11 @@ def generate_random_number():
 def check_guess():
     guess = int(guess_entry.get())
     if guess < number_to_guess:
-        result_label.config(text = 'Too low! Try again.')
+        result_label.config(text = "Too low! Try again.")
     elif guess > number_to_guess:
-        result_label.congig(text = 'Too high! Guess lower.')
+        result_label.congig(text = "Too high! Guess lower.")
     else:
-        messagebox.showinfo('Thats a bingo!', f"You correctly guessed the number in {num_guesses} attempts.")
+        messagebox.showinfo("Thats a bingo!", f"You correctly guessed the number in {num_guesses} attempts.")
         reset_game()
     
     guess_entry.delete(0, tk.END)
@@ -24,13 +24,30 @@ def reset_game():
     global number_to_guess, num_guesses
     number_to_guess = generate_random_number()
     num_guesses = 0
-    result_label.config(text='')
+    result_label.config(text="")
     guess_entry.delete(0, tk.END)
     guess_entry.focus()
 
 
-window= tk.TK()
-window.title('Guess the Number')
+window= tk.Tk()
+window.title("Guess the Number")
 
 number_to_guess = generate_random_number()
 num_guesses = 0
+
+instruction_label = tk.Label(window, text="Guess a number between 1 and 100:")
+instruction_label.pack()
+
+guess_entry = tk.Entry(window)
+guess_entry.pack()
+guess_entry.focus()
+
+check_button = tk.Button(window, text="Check", command=check_guess)
+check_button.pack()
+
+result_label = tk.Label(window, text="")
+result_label.pack()
+
+reset_game()
+
+window.mainloop()
